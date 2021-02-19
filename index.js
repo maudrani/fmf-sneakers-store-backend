5d35cf798e93898ccce6edbc83e08b6192d9a970
Mau <mauridrani95@gmail.com>
2021-02-18 22:03:41 -0300

    Port fixed again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extend: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 //Importar rutas
 app.use("/api/products", require("./routes/products"));
@@ -24,8 +24,8 @@ app.use("/api/mercadopago", require("./routes/mercadopago"));
 app.use("/api/sendmail", require("./routes/sendmail"));
 
 //Arrancar app
-app.listen(port, '0.0.0.0', () => {
-  console.log(`El servidor está funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`El servidor está funcionando en el puerto ${PORT}`);
 });
 
 // Definir la pagina principal
