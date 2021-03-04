1b558d1d551da310961ee8d581db6dd07d818329
Mau <mauridrani95@gmail.com>
2021-03-04 11:31:00 -0300

    Added notification url route

--- routes/mercadopago.js
+++ routes/mercadopago.js
@@ -3,15 +3,22 @@ const mercadopago = require("mercadopago");
 const express = require("express");
 const router = express.Router();
 
+/* TEST */
 mercadopago.configure({
   access_token:
     "TEST-1825194644671291-020305-941745df511e0bbbef675d8dc97bf9d3-181749895",
 });
 
+/* mercadopago.configure({
+  access_token:
+    "APP_USR-1825194644671291-020305-cb803dbe7220c03579bf51cd15e00004-181749895",
+}); */
+
 const CreatePreference = (req, res) => {
   mercadopago.preferences
     .create(req.body)
     .then(function (response) {
+      console.log(res.json({ ...response.body }));
       return res.json({ ...response.body });
     })
     .catch(function (error) {
@@ -20,6 +27,12 @@ const CreatePreference = (req, res) => {
     });
 };
 
-router.post("/", CreatePreference);
+const NotificationUrl = (req,res) => {
+  console.log(req);
+  console.log(res);
+}
+
+router.post("/createpreference", CreatePreference);
+router.post("/notificationurl", NotificationUrl);
 
 module.exports = router;
